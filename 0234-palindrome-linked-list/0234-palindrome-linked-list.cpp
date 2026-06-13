17ebd27ce055002c95016eb0711e1e833ab342bf
PREETHI <preethiprithivi05@gmail.com>
2026-06-13 21:27:00 +0530

    Time: 4 ms (50.42%), Space: 118 MB (64.03%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
-        
-        // Step 1: Find the middle of the linked list
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        
-        // Step 2: Reverse the second half of the linked list
-        ListNode* prev = nullptr;
-        ListNode* curr = slow;
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
         while (curr) {
             ListNode* nextNode = curr->next;
             curr->next = prev;
             prev = curr;
             curr = nextNode;
         }
-        
-        // Step 3: Compare the first half and the reversed second half
+
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* secondHalf = reverse(slow);
+
+        // Compare both halves
         ListNode* firstHalf = head;
-        ListNode* secondHalf = prev; // 'prev' is now the head of the reversed half
-        
-        while (secondHalf) { // We only need to check until the end of the second half
-            if (firstHalf->val != secondHalf->val) {
+
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val)
                 return false;
-            }
+
             firstHalf = firstHalf->next;
             secondHalf = secondHalf->next;
         }
-        
+
         return true;
     }
 };
